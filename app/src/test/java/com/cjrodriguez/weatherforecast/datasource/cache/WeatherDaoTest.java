7bd61bcb38c87622c3fc700d336f943cb59d5d55
Cj-Rodriguez101 <chibuikeoffu@gmail.com>
2023-08-02 01:05:26 +0100

    clean up WeatherDaoTest

--- app/src/test/java/com/cjrodriguez/weatherforecast/datasource/cache/WeatherDaoTest.java
+++ app/src/test/java/com/cjrodriguez/weatherforecast/datasource/cache/WeatherDaoTest.java
@@ -7,6 +7,7 @@ import com.cjrodriguez.weatherforecast.model.Current;
 import com.cjrodriguez.weatherforecast.model.Forecast;
 import com.cjrodriguez.weatherforecast.model.Location;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -16,73 +17,11 @@ import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WeatherDaoTest {
+    WeatherDao weatherDao;
 
-    //@Spy WeatherDao weatherDao;
-
-    @Test
-    public void testIfAllDataIsClearedOnTransaction_ExpectSuccess() {
-
-        WeatherDao weatherDao = Mockito.spy(new WeatherDao() {
-            @Override
-            Location getLocationData(String query) {
-                return null;
-            }
-
-            @Override
-            Current getCurrentSingleData() {
-                return null;
-            }
-
-            @Override
-            public List<Forecast> getForecastData() {
-                return null;
-            }
-
-            @Override
-            void deleteLocationData() {
-
-            }
-
-            @Override
-            void deleteCurrentWeatherData() {
-
-            }
-
-            @Override
-            void deleteForecastData() {
-
-            }
-
-            @Override
-            void insertLocation(Location locationList) {
-
-            }
-
-            @Override
-            void insertCurrent(Current Current) {
-
-            }
-
-            @Override
-            void insertForecast(List<Forecast> forecastList) {
-
-            }
-        });
-
-        weatherDao.clearAllData();
-
-        verify(weatherDao).deleteCurrentWeatherData();
-        verify(weatherDao).deleteLocationData();
-        verify(weatherDao).deleteForecastData();
-    }
-
-    //no need for tommorrow and future as they are very similar
-    @Test
-    public void testAndGetCurrentWeatherData_ExpectSuccess() {
-
-        //WeatherData weatherData = new WeatherData(new Location(TEST_CITY), null, null);
-
-        WeatherDao weatherDao = Mockito.spy(new WeatherDao() {
+    @Before
+    public void setup(){
+        weatherDao = Mockito.spy(new WeatherDao() {
             @Override
             Location getLocationData(String query) {
                 return null;
@@ -99,37 +38,39 @@ public class WeatherDaoTest {
             }
 
             @Override
-            void deleteLocationData() {
-
-            }
+            void deleteLocationData() {}
 
             @Override
-            void deleteCurrentWeatherData() {
-
-            }
+            void deleteCurrentWeatherData() {}
 
             @Override
-            void deleteForecastData() {
-
-            }
+            void deleteForecastData() {}
 
             @Override
-            void insertLocation(Location locationList) {
-
-            }
+            void insertLocation(Location locationList) {}
 
             @Override
-            void insertCurrent(Current Current) {
-
-            }
+            void insertCurrent(Current Current) {}
 
             @Override
-            void insertForecast(List<Forecast> forecastList) {
-
-            }
+            void insertForecast(List<Forecast> forecastList) {}
         });
 
-//        Mockito.when(weatherDao.getCurrentWeatherData(TEST_CITY)).thenReturn(weatherData);
+    }
+
+    @Test
+    public void testIfAllDataIsClearedOnTransaction_ExpectSuccess() {
+
+        weatherDao.clearAllData();
+
+        verify(weatherDao).deleteCurrentWeatherData();
+        verify(weatherDao).deleteLocationData();
+        verify(weatherDao).deleteForecastData();
+    }
+
+    //no need for tommorrow and future as they are very similar
+    @Test
+    public void testAndGetCurrentWeatherData_ExpectSuccess() {
         weatherDao.getCurrentWeatherData(TEST_CITY);
 
         verify(weatherDao).getLocationData(TEST_CITY);
